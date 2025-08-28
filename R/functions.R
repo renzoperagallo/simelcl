73e3c525026d942f7fc77709bdea5b210c6c86d2
Renzo Ignacio Peragallo Diaz <riperagallod@ine.gob.cl>
2025-08-28 15:47:30 -0400

    Se crean funciones para labelizar las tablas y se incluye en funcion que las simplifica. Tambien se crean funciones para ver la tabla de indicadores.

--- R/functions.R
+++ R/functions.R
@@ -32,9 +32,11 @@ get_data_url <- function(dataflow){
 
 #' Obtener url API SIMEL Chile (metadata)
 #'
-#' @param dataflow Un único valor de clase "character" con el nombre del dataflow (id en tabla indicadores).
+#' @param dataflow Un único valor de clase "character" con el nombre del
+#' dataflow (id en tabla indicadores).
 #'
-#' @returns Un único valor de clase "character" con la url de la API para la metadata.
+#' @returns Un único valor de clase "character" con la url de la API para la
+#' metadata.
 #' @export
 #'
 #' @examples
@@ -67,7 +69,8 @@ get_dsd_url <- function(dataflow){
 #'  Descarga información de una tabla de datos publicada en SIMEL.
 #'
 #'
-#' @param dataflow Un único valor de clase "character" con el nombre del dataflow (id en tabla indicadores).
+#' @param dataflow Un único valor de clase "character" con el nombre del
+#' dataflow (id en tabla indicadores).
 #' @param ... Otros argumentos de readSMDX
 #'
 #' @returns Un objeto de clase "data.frame" con los datos de una tabla en SIMEL.
@@ -89,7 +92,8 @@ get_data <- function(dataflow, ...){
 }
 
 #' Desagregaciones de SIMEL
-#' @returns Un vector de clase "character" con los nombres de las columnas de desagregación.
+#' @returns Un vector de clase "character" con los nombres de las columnas de
+#' desagregación.
 desagregaciones <- function(){
 
   out <- c(
@@ -129,7 +133,9 @@ desagregaciones <- function(){
 #' Simplificar tablas SUSESO
 #'
 #' @description
-#' Simplifica las tablas de SUSESO seleccionando las  columnas más importantes. Además, redondea  los valores al número de decimales establecidos en cada tablas.
+#' Simplifica las tablas de SUSESO seleccionando las  columnas más importantes.
+#' Además, redondea  los valores al número de decimales establecidos en cada
+#' tablas.
 #'
 #'
 #' @param df Objeto de clase "data.frame" proveniente de SIMEL.
@@ -155,6 +161,8 @@ simplificar_tabla <- function(df, redondear = TRUE){
 
   df <- timeperiod_to_date(df)
 
+  df <- labelizar_df(df)
+
   out <-
     df |>
     dplyr::select(
@@ -162,7 +170,6 @@ simplificar_tabla <- function(df, redondear = TRUE){
         c(
           "TIME_PERIOD",
           "AREA_REF",
-          "AREA_REF_label.es",
           desagregaciones(),
           "obsValue"
         )
@@ -184,7 +191,8 @@ simplificar_tabla <- function(df, redondear = TRUE){
 #'
 #' @param df Objeto de clase "data.frame" proveniente de SIMEL.
 #'
-#' @returns Un objeto de clase "data.frame" con la columna TIME_PERIOD como clase "date".
+#' @returns Un objeto de clase "data.frame" con la columna TIME_PERIOD como
+#' clase "date".
 #' @export
 #'
 #' @examples
@@ -251,7 +259,8 @@ timeperiod_to_date <- function(df){
 #'
 #' @param df Objeto de clase "data.frame" proveniente de SIMEL.
 #'
-#' @returns Objeto clase "Logical" con valor TRUE cuando el data.frame viene con fechas en formato trimestre movil.
+#' @returns Objeto clase "Logical" con valor TRUE cuando el data.frame viene
+#' con fechas en formato trimestre movil.
 #'
 #' @examples
 #' tbl <- data.frame(TIME_PERIOD = "2016-05-01/P3M")
@@ -266,4 +275,144 @@ identificar_trimestre_movil <- function(df){
     return(FALSE)
   }
 
+}
+
+#' Obtener lista indicadores.
+#'
+#' @description
+#' Retorna la lista de indicadores.
+#'
+#' @returns Objeto clase "data.frame" con la lista indicadores de SIMEL.
+#' @export
+#'
+#' @examples
+#' indicadores <- lista_indicadores()
+lista_indicadores <- function(){
+  return(tabla_indicadores)
+}
+
+#' Ver lista de indicadores
+#'
+#' @description
+#' Imprime la lista de indicadores.
+#'
+#' @returns Print de la lista de indicadores.
+#' @export
+#'
+#' @examples
+#' ver_lista_indicadores()
+ver_lista_indicadores <- function(){
+  print(tabla_indicadores, n = 1000)
+}
+
+#' Crea un diccionario de  valores de una columna.
+#'
+#' @param df Un objeto de clase "data.frame" con los datos de una tabla en SIMEL.
+#' @param col_codes Valor de clase "character"  con la columna con los códigos.
+#' @param col_labels Valor de clase "character" con la columna con las labels.
+#'
+#' @returns Un vector de clase "character" tipo "named vector" (diccionario).
+#'
+#' @examples
+#' tabla <- data.frame(levels = 1:3, labels = c("uno", "dos", "tres"))
+#' create_labels_dictionary(tabla, "levels", "labels")
+create_labels_dictionary <- function(df, col_codes, col_labels){
+
+  tabla <-
+    df |>
+    dplyr::distinct(
+      !!dplyr::sym(col_codes),
+      !!dplyr::sym(col_labels)
+    )
+
+  out <-
+    setNames(
+      dplyr::pull(tabla, !!dplyr::sym(col_codes)),
+      dplyr::pull(tabla, !!dplyr::sym(col_labels))
+    )
+
+  return(out)
+}
+
+# diccionario <- create_labels_dictionary(prueba, "AREA_REF", "AREA_REF_label.es")
+
+#' Labeliza una columna
+#'
+#' @description
+#' Permite transformar una columna a haven::labelled a partir de un diccionario.
+#'
+#' @param df Un objeto de clase "data.frame" con los datos de una tabla en SIMEL.
+#' @param col_name Objeto "character" con el nombre de la columna a transformar.
+#' @param labels_dictionary Objeto tipo named vector  utilizado como diccionario.
+#'
+#' @returns Objeto de clase "data.frame" con la columna  en formato
+#' haven::labelled.
+#' @export
+#'
+#' @examples
+#' tabla = data.frame(valores = 1:3)
+#' diccionario = c(1 = "uno", 2 = "dos", 3 = "tres")
+#' to_labelled(tabla, "valores", diccionario)
+to_labelled <- function(df, col_name, labels_dictionary){
+
+  out <-
+    df |>
+    mutate(
+      !!dplyr::sym(col_name) := haven::labelled(
+        !!dplyr::sym(col_name),
+        labels = labels_dictionary
+      )
+    )
+
+  return(out)
+
+}
+
+#' Labeliza una tabla de SIMEL
+#'
+#' @description
+#' Transforma en variables haven::labelled() las columnas que tienen una
+#' label asociada en las tablas SIMEL (excepto por columna DECIMAL).
+#'
+#'
+#' @param df Un objeto de clase "data.frame" con los datos de una tabla en SIMEL.
+#'
+#' @returns Un objeto de clase "data.frame" con las variables categóricas
+#' en formato haven::labelled.
+#'
+#' @export
+#'
+#' @examples
+#' tabla <- data.frame(numeros = 1:3, numeros_label.es = c("uno", "dos", "tres"))
+#' labelizar_df(tabla)
+labelizar_df <- function(df){
+
+  # Obtener columnas
+
+  nombres_columnas <- names(df)
+
+  nombres_columnas <- stringr::str_subset(nombres_columnas, "DECIMALS", negate = TRUE)
+
+  columnas_labels <- stringr::str_subset(nombres_columnas, "_label.es$")
+
+  columnas_codes <- stringr::str_extract(columnas_labels, "\\w+(?=_label.es)")
+
+  # Obtiene los diccionarios
+
+  diccionarios <-
+    purrr::map2(
+      columnas_codes,
+      columnas_labels,
+      ~create_labels_dictionary(prueba, .x, .y)
+    )
+
+  # Labeliza las columnas
+
+  for (i in 1:length(columnas_codes)){
+
+    df <- to_labelled(df, columnas_codes[[i]],  diccionarios[[i]])
+
+  }
+
+  return(df)
 }
