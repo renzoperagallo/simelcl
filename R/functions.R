0083099e88424282c5cd8cb21305833fd8b3c565
Renzo Ignacio Peragallo Diaz <riperagallod@ine.gob.cl>
2025-08-28 10:46:42 -0400

    Se agregan funciones que permiten convertir timeperiod en date. Se agregan estas funciones a simplificar_tabla().

--- R/functions.R
+++ R/functions.R
@@ -68,15 +68,16 @@ get_dsd_url <- function(dataflow){
 #'
 #'
 #' @param dataflow Un único valor de clase "character" con el nombre del dataflow (id en tabla indicadores).
+#' @param ... Otros argumentos de readSMDX
 #'
 #' @returns Un objeto de clase "data.frame" con los datos de una tabla en SIMEL.
 #' @export
 #'
 #' @examples
 #' get_data("TP_SEXO")
-get_data <- function(dataflow){
+get_data <- function(dataflow, ...){
 
-  sdmx.data <- rsdmx::readSDMX(get_data_url(dataflow))
+  sdmx.data <- rsdmx::readSDMX(get_data_url(dataflow), ...)
 
   sdmx.dsd <- rsdmx::readSDMX(file = get_dsd_url(dataflow))
 
@@ -134,6 +135,10 @@ desagregaciones <- function(){
 #' @param df Objeto de clase "data.frame" proveniente de SIMEL.
 #' @param redondear Valor lógico.
 #'
+#' @note
+#' En el caso de las tablas que utilizan la ENE, los trimestres móviles se
+#' transforma la variable fecha utilizando como referencia el mes central.
+#'
 #' @returns Un objeto de clase "data.frame" simplificado.
 #' @export
 #'
@@ -148,6 +153,8 @@ simplificar_tabla <- function(df, redondear = TRUE){
 
   # if(length(unique(df$INDICADOR))>1){stop("Hay más de un indicador")}
 
+  df <- timeperiod_to_date(df)
+
   out <-
     df |>
     dplyr::select(
@@ -158,9 +165,9 @@ simplificar_tabla <- function(df, redondear = TRUE){
           "AREA_REF_label.es",
           desagregaciones(),
           "obsValue"
-          )
         )
       )
+    )
   if(redondear== TRUE){
     out <- out |> dplyr::mutate(obsValue = round(obsValue, decimales))
   }
@@ -169,4 +176,94 @@ simplificar_tabla <- function(df, redondear = TRUE){
 
 }
 
+#' Transformar timeperiod a clase "date"
+#'
+#' @description
+#' Transforma la variable TIME_PERIOD a clase date.
+#'
+#'
+#' @param df Objeto de clase "data.frame" proveniente de SIMEL.
+#'
+#' @returns Un objeto de clase "data.frame" con la columna TIME_PERIOD como clase "date".
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' df <- get_data("TP_SEXO")
+#' df_v2 <- timeperiod_to_date(df)
+#' }
+timeperiod_to_date <- function(df){
 
+  if(unique(df$FREQ) == "M"){
+
+    if(identificar_trimestre_movil(df)){
+      out <-
+        df |>
+        dplyr::mutate(
+          TIME_PERIOD = lubridate::as_date(
+            stringr::str_sub(TIME_PERIOD, 1, 10)
+          ),
+          TIME_PERIOD = lubridate::add_with_rollback(
+            TIME_PERIOD,
+            months(1)
+          )
+        )
+    }else{
+      out <-
+        df |>
+        dplyr::mutate(
+          TIME_PERIOD = lubridate::as_date(
+            paste(
+              stringr::str_sub(TIME_PERIOD, 1, 4),
+              stringr::str_sub(TIME_PERIOD, 7, 8),
+              "01",
+              sep = "-"
+            )
+          )
+        )
+    }
+
+  }else if(unique(df$FREQ) == "A"){
+    out <-
+      df |>
+      dplyr::mutate(
+        TIME_PERIOD = lubridate::as_date(
+          paste(
+            stringr::str_sub(TIME_PERIOD, 1, 4),
+            "01",
+            "01",
+            sep = "-"
+          )
+        )
+      )
+  } else{
+    stop("No se encuentra la columna FREQ con un valor valido.")
+  }
+
+  return(out)
+}
+
+#' Identifica trimestre movil
+#'
+#' @description
+#' Identifica si la columna TIME_PERIOD  viene en formato de trimestre movil.
+#'
+#'
+#' @param df Objeto de clase "data.frame" proveniente de SIMEL.
+#'
+#' @returns Objeto clase "Logical" con valor TRUE cuando el data.frame viene con fechas en formato trimestre movil.
+#'
+#' @examples
+#' tbl <- data.frame(TIME_PERIOD = "2016-05-01/P3M")
+#' identificar_trimestre_movil(tbl)
+identificar_trimestre_movil <- function(df){
+
+  valor_prueba <- df$TIME_PERIOD[1] |> stringr::str_sub(-4, -1)
+
+  if(valor_prueba == "/P3M"){
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+
+}
